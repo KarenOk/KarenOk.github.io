26d46635043fac92c725eb7fd5fc6638e84eb3c5
Karen Okonkwo <karenokonkwo29@gmail.com>
2018-11-01 07:48:36 +0100

    feat: new shortcut icon for page. change: increased timer

--- classic-arcade-game/js/extras.js
+++ classic-arcade-game/js/extras.js
@@ -1,15 +1,21 @@
 let highScore = localStorage.getItem("highscore") || 0;
 let score = 0;
-let time = "00:10"
+let time = "01:10"
 let [min,sec] = time.split(":")
 let scoreElem = document.querySelector(".score");
 let highScoreElem = document.querySelector(".high-score");
 let timeElem = document.querySelector(".timer");
-timeElem.classList.remove("warning")
+let modal = document.querySelector(".modal");
+let replayElem = document.querySelector(".replay");
+let highScoreImg = document.querySelector(".high-score-img")
+let scoreDisplayElem = document.querySelector(".score-display")
 
-// function to manage scores
+timeElem.classList.remove("warning")
+highScoreImg.classList.add("hidden")
+
+// Function to manage scores
 function setScore(){
-    highScore = localStorage.getItem("highscore")
+    highScore = localStorage.getItem("highscore") || 0
     highScoreElem.innerHTML = highScore
     scoreElem.innerHTML = score
 };
@@ -24,15 +30,23 @@ Number.prototype.padNumber = function (size){
     return(paddedNo)
 }
 
-// function to manage timing of the gmae
+// Function to manage timing of the gmae
 function timer(){
     if (parseInt(min) === 0 && parseInt(sec) === 0){
-        // end games
-        cancelAnimationFrame(animationReq)
+        // End game
+        cancelAnimationFrame(animationReq);
+        scoreDisplayElem.innerHTML = "Score: " + score
+        modal.classList.remove("hidden") // show modal
+
+        // Check if score is a high score and show high scoreimage
+        if (score.toString() === localStorage.getItem("highscore")){
+            highScoreImg.classList.remove("hidden")
+        }
 
     } else if(parseInt(sec) === 0){
         min = (parseInt(min) - 1).padNumber(2);
         sec = "59"
+
     } else{
         sec = (parseInt(sec) - 1).padNumber(2)
     }
@@ -46,4 +60,11 @@ function timer(){
 
 setInterval(timer, 1000)
 
-console.log(animationReq)
+
+
+// Handle replay
+replayElem.addEventListener("click", function(){
+    window.location.reload()
+    modal.classList.add("hidden")
+})
+
